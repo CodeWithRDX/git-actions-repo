9aa62d19c11300661b9b341d356e3f879300f312
CodeWithRDX <Codewithrdx@gmail.com>
2026-05-17 22:03:51 +0530

    new route added

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
   res.send('Health is fine! Again');
 })
+app.get('/about', (req, res) => {
+  res.send('About us! Again');
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
