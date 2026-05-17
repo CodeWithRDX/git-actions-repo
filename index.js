2d397fc76b14a198d12878811782c6f521022a62
CodeWithRDX <Codewithrdx@gmail.com>
2026-05-17 20:59:15 +0530

    SSH setup 3

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! Again');
 })
 app.get('/health', (req, res) => {
-  res.send('Health is fine');
+  res.send('Health is fine! Again');
 })
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
